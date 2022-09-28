7617edc6402476147271f8da6ad4a34556d94151
Miroslav Holec <mirek@miroslavholec.cz>
2022-09-28 14:36:53 +0200

    Global updates

--- blazor-error-boundary.cs
+++ blazor-error-boundary.cs
@@ -1,10 +1,16 @@
 // MainLayout.razor (obvykle)
 
-<ErrorBoundary>
+<ErrorBoundary @ref="_errorBoundary">
     <ChildContent>
         @Body
     </ChildContent>
-    <ErrorContent>
+    <ErrorContent Context="exception">
         <p>Error content nebo komponenta</p>
+        <code>@exception.ToString()</code>
+        <p @onclick="() => _errorBoundary.Recover()">Zotavit</p>
     </ErrorContent>
 </ErrorBoundary>
+
+@code {
+    private ErrorBoundary _errorBoundary;
+}
